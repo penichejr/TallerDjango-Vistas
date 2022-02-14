86f75c196ec2a38da3ce377f6fe34e8740de7401
Javier Peniche <jd.peniche@uniandes.edu.co>
2022-02-13 22:44:43 -0500

    con nueva bd

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "bdprueba",
+        "USER": "javie",
+        "PASSWORD": "mona200101",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
